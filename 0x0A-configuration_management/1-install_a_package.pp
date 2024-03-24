95ea1474772df180584d5379f0e8554672bea854
ojijoebrain <ojijosephoji@gmail.com>
2024-03-23 21:15:34 -0700

    task_1_again

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
+# Manage Flask package using pip
+python::pip { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
