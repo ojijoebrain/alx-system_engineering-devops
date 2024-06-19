e82bc01e3063e5fb1619b5b7f75ca35a752171c9
ojijoebrain <ojijosephoji@gmail.com>
2024-06-19 18:27:26 +0100

    task_0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on Reddit subreddit.
+Script that queries subscribers on a Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return total num of subscribers on given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get('data', {}).get('subscribers', 0)
         return subscribers
-    else:
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
         return 0
+    except ValueError:
+        print("Error decoding JSON response")
+        return 0
+
+
+if __name__ == "__main__":
+    subreddit = input("Enter the name of the subreddit: ")
+    subscribers = number_of_subscribers(subreddit)
+    print(f"Number of subscribers in r/{subreddit}: {subscribers}")
