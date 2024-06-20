c10bacab22434b4fa3852ac677413f5add26cc04
ojijoebrain <ojijosephoji@gmail.com>
2024-06-20 21:40:22 +0100

    task_1_updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a Reddit subreddit.
+A script that queries the Reddit API and returns the number of subscribers.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    A function that queries the Reddit API and
+    returns the number of subscribers.
+    """
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data.get('data', {}).get('subscribers', 0)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code == 200:
+        result = res.json()
+        subscribers = result['data']['subscribers']
         return subscribers
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    else:
         return 0
-    except ValueError:
-        print("Error decoding JSON response")
-        return 0
-
-
-if __name__ == "__main__":
-    subreddit = input("Enter the name of the subreddit: ")
-    subscribers = number_of_subscribers(subreddit)
-    print(f"Number of subscribers in r/{subreddit}: {subscribers}")
